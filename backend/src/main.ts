bab8ecaf8050f6778827e9b56e0d92652af9159a
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-08-13 02:07:44 +0000

    import helmet, morgan and cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
