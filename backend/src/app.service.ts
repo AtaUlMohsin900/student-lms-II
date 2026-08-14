36b8b16aacaf6db5c9c23abe11088e3cc6878983
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2026-08-14 11:38:59 +0500

    update

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { successResponse } from './common/http/response.util';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(private readonly DataSource: DataSource) {}
+
+
+  async onModuleInit() {
+    try {
+      await this.DataSource.query('SELECT 1');
+      this.logger.log('DataSource has been initialized successfully.');
+    } catch (error) {
+      this.logger.error('Error occurred while initializing DataSource', error);
+    }
+  }
+
+  getRootPayload(){
+      return successResponse('Student LMS API is running successfully',{
+        version: '1.0.0',
+        message: 'Welcome to the Student LMS API',
+        status: 'success',
+        timestamp: new Date().toISOString(),
+      });
+  }
+
+  getHealthPayload() {
+    return successResponse('Student LMS API is healthy', {
+      status: 'OK',
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString(),
+    });
   }
 }
