d6eadd2e17f5c74bc419ba1e2bf12c5a921a1bbd
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2026-08-19 07:35:34 +0500

    update

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import { BeforeInsert, PrimaryGeneratedColumn } from "typeorm";
+
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @BeforeInsert()
+    ensureInsertDefaults(){
+        if(!this.id){
+            this.id = randomUUID();
+        }
+
+        const now = new Date();
+        if(!this.createdAt){
+            this.createdAt = now;
+        }
+        if(!this.updatedAt){
+            this.updatedAt = now;
+        }
+    }
+    @BeforeUpdate()
+    touchUpdateAt(){
+        this.updateAt = new Date();
+    }
+
+    @Column({type:'varchar', length: 100})
+    name: string;   
+    
+    @Column({type:'varchar', length: 255, unique: true)
+    email: string;
+ 
+    @Column({name:'password_hash', type:'varchar', length: 255, nullable: true})
+    passwordHash: string | null;
+} 
