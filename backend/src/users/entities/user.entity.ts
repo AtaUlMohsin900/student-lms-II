3a333b5b755df940878b92460a61e929df1857bb
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2026-08-20 07:34:13 +0500

    create database

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,8 +1,10 @@
-import { BeforeInsert, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, PrimaryGeneratedColumn } from "typeorm";
+import { UserRole, UserStatus } from "../enums/users.enms";
+import { randomUUID } from "crypto";
 
 export class User {
     @PrimaryGeneratedColumn('uuid')
-    id: string;
+    id!: string;
 
     @BeforeInsert()
     ensureInsertDefaults(){
@@ -20,15 +22,69 @@ export class User {
     }
     @BeforeUpdate()
     touchUpdateAt(){
-        this.updateAt = new Date();
+        this.updatedAt = new Date();
     }
 
     @Column({type:'varchar', length: 100})
-    name: string;   
+    name!: string;   
     
-    @Column({type:'varchar', length: 255, unique: true)
-    email: string;
+    @Column({type:'varchar', length: 255, unique: true})
+    email!: string;
  
-    @Column({name:'password_hash', type:'varchar', length: 255, nullable: true})
-    passwordHash: string | null;
-} 
+    @Column({
+        name:'password_hash', 
+        type:'varchar', 
+        length: 255, 
+        nullable: true
+    })
+    passwordHash!: string | null;
+
+    @Column({type:'enum',enum:UserRole, default:UserRole.STUDENT})
+    roll!: UserRole;
+    
+    @Column({type:'enum',enum:UserStatus, default:UserStatus.PENDING})
+    status!: UserStatus;
+
+    @Column({
+        name:'profile_picture_url', 
+        type:'varchar', 
+        length: 500, 
+        nullable: true
+    })
+    profilePictureUrl!: string | null;
+
+    @Column({type:'varchar', length: 20, nullable: true})
+    phone!: string | null;
+
+    @Column({name:'date_of_brith', type:'date', nullable: true})
+    dateOfBrith!: string | null;
+
+    @Column({
+        name:'google_id', 
+        type:'varchar', 
+        length: 255,
+        unique: true, 
+        nullable: true
+    })
+    googleId!: string | null;
+
+    @Column({name:'email_verified', type:'boolean', default: false})
+    emailVerified!: boolean;
+
+    @Column({name:'last_login', type:'timestamp', nullable: true})
+    lastLogin!: Date | null;
+
+    @Column({name:'last_learning_date', type:'date', nullable: true})
+    lastLearningDate!: string | null;
+
+    @Column({name:'current_streak', type:'int', default: 0})
+    currentStreak!: number;
+
+    
+    @Column({name:'created_at', type:'timestamp'})
+    createdAt!: Date;
+
+    @Column({name:'updated_at', type:'timestamp'})
+    updatedAt!: Date;
+}  
+ 
---
+++ backend/src/users/enums/users.enms.ts
@@ -0,0 +1,12 @@
+export enum UserRole{
+    SUPER_ADMIN= 'super_admin',
+    INSTUCTOR= 'instuctor',
+    STUDENT= 'student'
+}
+
+export enum UserStatus{
+    ACTIVE='active',
+    PENDING='pending',
+    SUSPENDED='suspended',
+    BANNED='banned'
+}
