041b226d043a9373176bd9941a3efba1ea0b548b
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2026-08-27 07:18:25 +0500

    create validator of instructor-application.dto

--- backend/src/users/dto/instructor-application.dto.ts
+++ backend/src/users/dto/instructor-application.dto.ts
@@ -1,4 +1,5 @@
-import { IsArray, IsOptional, IsString } from "class-validator";
+import { Transform } from "class-transformer";
+import { IsArray, IsInt, IsOptional, IsString, IsUrl, MaxLength, Min } from "class-validator";
 
 export class InstructorApplicationDto {
     @IsString()
@@ -9,8 +10,32 @@ export class InstructorApplicationDto {
     expertiseAreas?: string[]
 
     @IsOptional()
+    @IsInt()
+    @Min(0)
+    experienceYears?: number
+
+    @IsOptional()
     @IsString()
     @Transform(({ value }) => (value === '' ? null : value))
     education?: string
 
-} 
+    @IsOptional()
+    @IsUrl()
+    @MaxLength(500)
+    @Transform(({ value }) => (value === '' ? null : value))
+    portfolioUrl?: string
+
+    @IsOptional()
+    @IsUrl()
+    @MaxLength(500)
+    @Transform(({ value }) => (value === '' ? null : value))
+    linkedinUrl?: string
+
+    @IsOptional()
+    @IsUrl()
+    @MaxLength(500)
+    @Transform(({ value }) => (value === '' ? null : value))
+    githubUrl?: string
+
+
+}
