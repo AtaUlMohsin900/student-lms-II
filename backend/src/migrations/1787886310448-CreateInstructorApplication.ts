0b41ca07784898cdfb97b0546e2f673bd12cdb3b
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-08-29 02:42:19 +0000

    compete all enums, dto, users

--- backend/src/migrations/1787886310448-CreateInstructorApplication.ts
+++ backend/src/migrations/1787886310448-CreateInstructorApplication.ts
@@ -3,14 +3,14 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class CreateInstructorApplication1787886310448 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "InstructorApplications_status_enum"AS ENUM('pending', 'approved', 'rejected')`)
+        await queryRunner.query(`CREATE TYPE "instructorApplications_status_enum"AS ENUM('pending', 'approved', 'rejected')`)
         await queryRunner.createTable(
             new Table({
                 name: 'InstructorApplications',
                 columns: [
                     { name: 'id', type: 'uuid', isPrimary: true, default: 'uuid_generate_v4()' },
                     { name: 'user_id', type: 'uuid', isUnique: true },
-                    { name: 'status', type: 'InstructorApplications_status_enum', default: 'pending' },
+                    { name: 'status', type: 'instructorApplications_status_enum', default: 'pending' },
                     { name: 'bio', type: 'text'},
                     { name: 'expertise_areas', type: 'text',isArray: true, default: "'{}'" },
                     { name: 'experience_years', type: 'int', isNullable: true },
@@ -41,9 +41,12 @@ export class CreateInstructorApplication1787886310448 implements MigrationInterf
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        const table = await queryRunner.getTable('Instructor_applications');
+        const fk = table?.foreignKey.find((k)=> k.columnNames.indexOf('user_if')!== -1);
+        if(fk) await queryRunner.dropForeignKey('Instructor_applications', fk)
         await queryRunner.dropTable('InstructorApplications', true);
-        await queryRunner.query(`DROP TYPE IF EXISTS "users_role_enum"`);
-        await queryRunner.query(`DROP TYPE IF EXISTS "users_status_enum"`);
+        await queryRunner.query(`DROP TYPE IF EXISTS "instructorApplications_status_enum"`);
+        
     }
 
 }
