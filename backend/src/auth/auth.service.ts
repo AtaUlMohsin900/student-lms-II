8ecf39bddd2e3dae9827f828741468508f771155
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2026-09-03 07:20:02 +0500

    update in auth.service.ts passwordhas using bcrypt

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { UserEntity } from '../../src/user/entities/user.entity';
 import { InstuctorApplicationEntity } from '../../src/InstuctorApplicationEntity';
 import { Repository } from 'typeorm';
@@ -7,6 +7,7 @@ import { Repository } from 'typeorm';
 import { SignupDto } from './dto/signup.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
+import bcrypt from 'node_modules/bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -22,12 +23,14 @@ export class AuthService {
   async signup(dto: SignupDto) {
     // step 1: Restrict for dubling email accounts.
     const existingUser = await this.userRepository.findOne({
-      where: { email: dto.mail },
+      where: { email: dto.email },
     })
     if (existingUser) {
-      throw new ConflictExeption('Email already exists');
+      throw new ConflictException('Email already exists');
 
     }
+    const passwordhash = await bcrypt.hash(dto.password, 12);
+    return passwordhash;
 
   }
 }
