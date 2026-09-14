2fc8114a42ccba955ad157b595483f2b90fe7e49
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2026-09-14 07:26:38 +0500

    update in auth.module.ts file

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { GoogleStrategy } from './strategies/google.startegy';
+import { TypeOrmMudule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { InstructorApplicationEntity } from 'src/users/entities/instuctor-application.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [
+    ConfigModule,
+    PassportModule.register({ session: true }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFectory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN', '24d') as any
+        }
+      })
+    }),
+    TypeOrmMudule.forFeature([UserEntity, InstructorApplicationEntity])
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, GoogleStrategy],
 })
-export class AuthModule {}
+export class AuthModule { }
