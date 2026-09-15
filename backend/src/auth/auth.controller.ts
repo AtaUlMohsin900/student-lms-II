388f93aef7011c2901de60f2a6fecf490d3ec262
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2026-09-15 07:36:11 +0500

    create post request in auth.controller file

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,20 +1,25 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
+import { successResponse } from '../common/http/response.util';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Post('signup')
-  signup(@Body() dto: SignupDto) {
-    return this.authService.signup(dto);
+  async signup(@Body() dto: SignupDto) {
+    const data = await this.authService.signup(dto);
+    return successResponse('User registered successfully', data);
   }
 
   @Post('login')
-  login(@Body() dto: LoginDto) {
-    return this.authService.login(dto);
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() dto: LoginDto) {
+    const data = await this.authService.login(dto);
+    return successResponse('Login successfully', data);
   }
 
   @Get('me/:id')
