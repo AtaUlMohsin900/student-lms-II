76cad51ca3fd2da11bb031e289fb46578c07d8cd
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2026-09-23 06:59:13 +0500

    create jwt.auth.guard.ts file

---
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { AuthGuard } from "@nestjs/passport";
+import { Observable } from "rxjs";
+
+
+
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(private readonly reflector: Reflector) { }
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    CanActivate(context: ExecutionContext): boolean {
+
+    }
+
+}
