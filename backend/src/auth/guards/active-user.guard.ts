df3b2bc935d2b44dd677c795931ec051d1651eb7
AtaUlMohsin900 <ataulmohsin900@gmail.com>
2026-09-23 08:05:21 +0500

    active.user. gaurd.ts file created

---
+++ backend/src/auth/guards/active-user.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { UserStatus } from "src/users/enums/users.enms";
+
+
+
+
+
+@Injectable()
+export class ActiveUserGaurd implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+        const request = context.switchToHttp()
+            .getRequest<{ user?: { status?: UserStatus } }>();
+
+        const status = request.user?.status;
+        if (status && status !== UserStatus.ACTIVE) {
+            throw new ForbiddenException('Your account is not active. Please contact the administrator.');
+
+        }
+        return true;
+
+    }
+}
